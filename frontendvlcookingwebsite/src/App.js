b7668123d8e80763e4d6f3b3778d9d0430de7cff
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-06-15 21:09:50 +0700

    xong home page ( tạo 1 data Json, 1 page item cho Home page )

--- frontendvlcookingwebsite/src/App.js
+++ frontendvlcookingwebsite/src/App.js
@@ -1,15 +1,20 @@
-import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./pages/About";
 import Home from "./pages/Home";
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-			<Home />
-			<Footer />
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontendvlcookingwebsite/src/components/Header.js
+++ frontendvlcookingwebsite/src/components/Header.js
@@ -1,27 +1,42 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-	return (
-		<header className="bg-slate-300 shadow-sm">
-			<div className="flex justify-between items-center mx-auto p-3">
-				<h1 className="font-bold text-xl">VlCooking</h1>
-				{/* SearchBar */}
-				<form className="bg-slate-100 p-3 rounded-lg flex items-center">
-					<FaSearch className="text-slate-500 mr-2" />
-					<input
-						className="bg-transparent focus:outline-none"
-						type="text"
-						placeholder="Search..."
-					/>
-				</form>
-				<ul className="flex justify-between gap-5 font-bold items-center">
-					<li className="hidden sm:inline">About</li>
-					<li className="hidden sm:inline">Login</li>
-					<li className="hidden sm:inline">Sign Up</li>
-					<button className="bg-slate-600 text-white rounded-lg p-1">Logout</button>
-				</ul>
-			</div>
-		</header>
-	);
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">VLU</span>
+            <span className="text-slate-700">Cooking</span>
+          </h1>
+        </Link>
+        {/* SearchBar */}
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/" className="">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/signin">
+            <li className=" text-slate-700 hover:underline">Sign in</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
 }
