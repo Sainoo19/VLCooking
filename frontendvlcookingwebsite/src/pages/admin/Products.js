12cc5f4faa6409a91127ac80065c083e88d50da5
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-06-23 18:13:05 +0700

    đã chỉnh sửa css của role và chỉnh sửa lại h1 của từng page cho nó vào  giữa

--- frontendvlcookingwebsite/src/pages/admin/Products.js
+++ frontendvlcookingwebsite/src/pages/admin/Products.js
@@ -5,29 +5,31 @@ import Sidebar from "../../components/Sidebar";
 import Table from "../../components/Table";
 
 export default function Products() {
-	return (
-		<div className="flex max-w-full overflow-x-auto sm:text-lg">
-			<Sidebar />
-			<div className=" max-w-6xl  p-3 sm:text-lg">
-				<h1 className="text-3xl font-bold">Manage Products</h1>
-				<div className="flex flex-row-reverse gap-4 w-full">
-					<form className="bg-slate-100 w-78 p-3 rounded-lg flex items-center mr-4">
-						<input
-							type="text"
-							placeholder="Search..."
-							className="bg-transparent focus:outline-none min-w-72   sm:w-24"
-						/>
-						<FaSearch className="text-slate-600 pl-1" />
-					</form>
-					<Link
-						to="/admin/create/product"
-						className="flex items-center justify-center rounded-lg min-h-[48px] bg-cyan-300 px-3 py-1 text-center hover:bg-black hover:text-white"
-					>
-						Create New
-					</Link>
-				</div>
-				<Table Name="Product" />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex max-w-full overflow-x-auto sm:text-lg">
+      <Sidebar />
+      <div className=" max-w-6xl  p-3 sm:text-lg">
+        <div className="flex flex-col items-center mx-2">
+          <h1 className="text-3xl font-bold">Manage Products</h1>
+        </div>
+        <div className="flex flex-row-reverse gap-4 w-full my-5">
+          <form className="bg-slate-100 w-78 p-3 rounded-lg flex items-center mr-4">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="bg-transparent focus:outline-none min-w-72   sm:w-24"
+            />
+            <FaSearch className="text-slate-600 pl-1" />
+          </form>
+          <Link
+            to="/admin/create/product"
+            className="flex items-center justify-center rounded-lg min-h-[48px] bg-cyan-300 px-3 py-1 text-center hover:bg-black hover:text-white"
+          >
+            Create New
+          </Link>
+        </div>
+        <Table Name="Product" />
+      </div>
+    </div>
+  );
 }
